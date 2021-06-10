2b29830433f5538ccc81f81d589b8b986c116873
thatblindgeye <thatblindgeye@gmail.com>
2021-06-10 18:45:56 -0400

    Fix misplacement of aria attributes on scoreboard

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -3,17 +3,21 @@ import React from 'react';
 export default function Scoreboard({ currentScore, highScore }) {
   return (
     <>
-      <div className='c-scoreboard__score'>
-        <span
-          className='c-scoreboard__name'
-          aria-live='polite'
-          aria-atomic='true'
-        >
+      <div
+        className='c-scoreboard__score'
+        aria-live='polite'
+        aria-atomic='true'
+      >
+        <span className='c-scoreboard__name'>
           <strong>Current Score: </strong>
         </span>
         <span className='c-scoreboard__number'>{currentScore}</span>
       </div>
-      <div className='c-scoreboard__score'>
+      <div
+        className='c-scoreboard__score'
+        aria-live='polite'
+        aria-atomic='true'
+      >
         <span
           className='c-scoreboard__name'
           aria-live='polite'
